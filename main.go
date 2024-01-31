519be5cdb50ce16d123337ecafd927a6153259c2
suman chapai <sumanchapai@gmail.com>
2024-01-31 16:29:34 +0545

    fix: unsed version import

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 )
 
+var Version = "0.0.0"
+
 func main() {
+	// Version
+	versionPtr := flag.Bool("version", false, "version")
+	flag.Parse()
+	if *versionPtr {
+		fmt.Println(Version)
+		return
+	}
+	// Normal
 	for _, msg := range os.Args[1:] {
 		fmt.Printf("%v", msg)
 	}
